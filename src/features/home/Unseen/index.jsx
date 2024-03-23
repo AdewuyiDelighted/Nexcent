868ee550b4014c667c5ecf720389aed1d73fab02
delighted <deborahdelighted5@gmail.com>
2024-03-23 17:30:00 +0100

    first commit

---
+++ src/features/home/Unseen/index.jsx
@@ -0,0 +1,15 @@
+import style from "./index.module"
+import image from "../../../asset/unforseen/Frame 35.png"
+
+
+const Unforseen = () =>{
+    return(
+        <div className={style.mainBoard}>
+            <div className={style.image}>
+                <img src={}/>
+            </div>
+        </div>
+    )
+    
+}
+export default Unforseen
